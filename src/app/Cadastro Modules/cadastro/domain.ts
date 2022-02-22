5cc7a1c394a2cd73e45c1badfe37cf7cfa575613
Eric Santos da Silva <ericss@primeit.com.br>
2022-02-22 11:55:21 -0300

    Passando arquivo

---
+++ src/app/Cadastro Modules/cadastro/domain.ts
@@ -0,0 +1,9 @@
+export interface cadastro{
+    cpfCnpj: number;
+    senha: string
+    nome: string
+    sobrenome: string
+    razaoSocial: string
+    dtNascFund: Date
+    dtCriacao: Date;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './Cadastro Modules/cadastro/cadastro.component';
+import { CadastroService } from './cadastro.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [CadastroService,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
